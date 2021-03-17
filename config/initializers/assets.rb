1f87ce79db92969e5ebef8dbfaf2fd9c88071ac5
Sefux <sfuchs@projektkater.de>
2021-03-17 14:15:39 +0100

    Fix asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,6 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 
 Rails.application.config.assets.paths << Rails.root.join('assets/favicon')
-Rails.application.config.assets.paths << Rails.root.join('assets/images')
 Rails.application.config.assets.paths << Rails.root.join('vendor/assets/img')
 Rails.application.config.assets.paths << Rails.root.join('vendor/assets/fonts')
 
