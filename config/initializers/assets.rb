6b387d4b26754b95b780390028ab1bbc35cba2dc
Sefux <sfuchs@projektkater.de>
2021-03-17 13:31:59 +0100

    Testing asset pipeline for images

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,7 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 
 Rails.application.config.assets.paths << Rails.root.join('assets/favicon')
+Rails.application.config.assets.paths << Rails.root.join('assets/images')
 Rails.application.config.assets.paths << Rails.root.join('vendor/assets/img')
 Rails.application.config.assets.paths << Rails.root.join('vendor/assets/fonts')
 
