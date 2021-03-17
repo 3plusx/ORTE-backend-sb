39fae10f840f8be284fea84915bb849f6289694d
Sefux <sfuchs@projektkater.de>
2021-03-17 11:56:58 +0100

    Add model validation

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,4 +2,10 @@
 
 class Submission < ApplicationRecord
   belongs_to :place, optional: true
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, email: true, on: :create
+  validates :rights, inclusion: [true]
+  validates :privacy, inclusion: [true]
+  validates :locale, presence: true
 end
