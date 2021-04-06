c68b56e6b5dc2ba6ce9fd6661696192ed7a8d6dd
Sefux <sfuchs@projektkater.de>
2021-04-06 18:41:04 +0200

    Add email to notifications

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -6,7 +6,7 @@ if Rails.env.production? || Rails.env.staging?
     email: {
       email_prefix:           "[ORTE Backend - #{::Rails.env}] ",
       sender_address:         %{ technik@3plusx.de  },
-      exception_recipients:   %w{ technik@3plusx.de   }
+      exception_recipients:   %w{ technik@3plusx.de sebastian.fuchs@3plusx.de  }
   }
 
 end
