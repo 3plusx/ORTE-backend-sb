c64871af4a8bcc4978509593fb4cda2857fedf3a
Ulf Treger <ulf.treger@googlemail.com>
2021-04-08 17:18:17 +0200

    Hotfix image helper

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,14 +1,18 @@
 module ImagesHelper
   def image_url(file)
-    filename = ActiveStorage::Blob.service.path_for(file.key)
-    if file.attached? && File.exist?(filename)
-      polymorphic_url(file.variant(resize: "800x800").processed)
+    if file.attached?
+      filename = ActiveStorage::Blob.service.path_for(file.key)
+      if  File.exist?(filename)
+        polymorphic_url(file.variant(resize: "800x800").processed)
+      end
     end
   end
   def image_linktag(file,title='')
-    filename = ActiveStorage::Blob.service.path_for(file.key)
-    if file.attached? && File.exist?(filename)
-      "<img src=\"#{polymorphic_url(file.variant(resize: "800x800").processed)}\" title=\"#{(title.present? ? title : '')}\">".html_safe
+    if file.attached?
+      filename = ActiveStorage::Blob.service.path_for(file.key)
+      if File.exist?(filename)
+        "<img src=\"#{polymorphic_url(file.variant(resize: "800x800").processed)}\" title=\"#{(title.present? ? title : '')}\">".html_safe
+      end
     end
   end
 end
