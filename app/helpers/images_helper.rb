52ad9abed6ae53f8ce0c03c0553e12e9e9a212f4
Ulf Treger <ulf.treger@googlemail.com>
2021-04-08 15:07:55 +0200

    Fix and externd GeoJSON Export, address data + images

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,11 +1,13 @@
 module ImagesHelper
   def image_url(file)
-    if file.attached?
+    filename = ActiveStorage::Blob.service.path_for(file.key)
+    if file.attached? && File.exist?(filename)
       polymorphic_url(file.variant(resize: "800x800").processed)
     end
   end
   def image_linktag(file,title='')
-    if file.attached?
+    filename = ActiveStorage::Blob.service.path_for(file.key)
+    if file.attached? && File.exist?(filename)
       "<img src=\"#{polymorphic_url(file.variant(resize: "800x800").processed)}\" title=\"#{(title.present? ? title : '')}\">".html_safe
     end
   end
